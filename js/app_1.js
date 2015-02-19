a73884b606f6c5ce9e51b1dfaa07e6be416e688b
BStanke <BStanke@users.noreply.github.com>
2015-02-19 15:09:08 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'bstanke.l8mm4ip6'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1IjoiYnN0YW5rZSIsImEiOiJMZXFweUdRIn0.fQNY4-tsvGPi6trhZJD4fg'; //<- this is my token, use yours.
+var mapId = 'bstanke.l8mm4ip6'; //<- this references the ugly green map that I made (now mine)
+var token = 'pk.eyJ1IjoiYnN0YW5rZSIsImEiOiJMZXFweUdRIn0.fQNY4-tsvGPi6trhZJD4fg'; //<- this is my token, use yours (done).
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
@@ -12,5 +12,19 @@ var map = L.mapbox.map('map', mapId);
 map.setView([39, -96], 4);
 
 //Add Data
-var dataFileToAdd = 'data/restaurants.geojson':
+var dataFileToAdd = 'data/overpass.geojson';
+
+var featureLayer = L.mapbox.featureLayer();
+    
+    featureLayer.loadUrl(dataFileToAdd);
+    featureLayer.addTo(map);
+
+featureLayer.on('ready' function(){
+    this.setStyle(){
+      "marker-color": "#777777",
+      "marker-size": "medium"
+    });
+    map.fitBounds(featureLayer.getBounds());
+});
+
 
